a9c3668fac8b2e3a115383f46063930f807a8ccb
Patrícia Pedrosa <patricia.pedrosa.1996@hotmail.com>
2018-06-26 01:06:57 +0100

    gateway,ip,ssid in list

--- resources/views/thing/list.blade.php
+++ resources/views/thing/list.blade.php
@@ -2,73 +2,81 @@
 
 
 <main class="main">
-<div class="container">
-    <div class="card-body">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">
-                    <h3><img src="{{ asset('img/iot3.png')}}" height="40" width="40"></img> Things
-                        <a class="btn btn-xs btn-success" style="float:right" href="{{ route('thing.create')}}">Configurar Things</a>
-                    </h3>
-                </div>
-                @if (count($things))
-                @if(session('success'))
-                @include('partials.success')
-                @endif
-                <table class="table table-striped">
+    <div class="container">
+        <div class="card-body">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <h3><img src="{{ asset('img/iot3.png')}}" height="40" width="40"></img> Things
+                            <a class="btn btn-xs btn-success" style="float:right" href="{{ route('thing.create')}}">Configurar Things</a>
+                        </h3>
+                    </div>
+                    @if (count($things))
+                    @if(session('success'))
+                    @include('partials.success')
+                    @endif
+                    <table class="table table-striped">
 
 
-                    <thead>
+                        <thead>
+                            <tr>
+                                <th>MAC</th>
+                                <th>IP</th>
+                                <th>Gateway</th>
+                                <th>SSID</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        @foreach ($things as $thing)
                         <tr>
-                            <th>MAC</th>
-                            <th>Configurado</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    @foreach ($things as $thing)
-                    <tr>
 
-                        <td>
-                            {{$thing->mac}}
-                        </td>
+                            <td>
+                                {{$thing->mac}}
+                            </td>
 
-                        <td>
-                            {{$thing->ConfiguradoToStr()}}
-                        </td>
-                        
-                        <td>
-                            <a class="btn btn-info" href="{{ route('thing.showthing', $thing) }}">Ver Thing</a>
+                            <td>
+                                {{$thing->ip}}
+                            </td>
+                            <td>
+                                {{$thing->gateway}}
+                            </td>
+                            <td>
+                                {{$thing->ssid}}
+                            </td>
+                            
+                            <td>
+                                <a class="btn btn-info" href="{{ route('thing.showthing', $thing) }}">Ver Thing</a>
 
-                            <a class="btn btn-info" href="{{route('sensor.list', $thing->id)}}">Ver Sensores</a>
-                        
-                            <a class="btn btn-success" href="{{ route('thing.edit',$thing->id) }}">Editar</a>
-                        
-                            <a><form action="{{ route('thing.delete',$thing) }}" method="POST" accept-charset="utf-8">
-                                {{method_field('delete')}}
-                                {{csrf_field()}}
-                                <button type="submit" class="btn btn-xs btn-danger">Remover</button>
-                            </form> </a>
+                                <a class="btn btn-info" href="{{route('sensor.list', $thing->id)}}">Ver Sensores</a>
+                                
+                                <a class="btn btn-success" href="{{ route('thing.edit',$thing->id) }}">Editar</a>
+                                
+                                <a><form action="{{ route('thing.delete',$thing) }}" method="POST" accept-charset="utf-8">
+                                    {{method_field('delete')}}
+                                    {{csrf_field()}}
+                                    <button type="submit" class="btn btn-xs btn-danger">Remover</button>
+                                </form> </a>
 
                             </td>
-                        
+                            
 
-                    </tr>
-                    @endforeach
+                        </tr>
+                        @endforeach
 
-                </table>
-                @else
-                <div class="card-body">
+                    </table>
+                    @else
+                    <div class="card-body">
 
-                <h2>Não foram encontrados things</h2>
-            </div>
-                @endif 
-                <div class="row justify-content-center">
-                    <div class="pagination" align="middle"> {{$things->links()}}</div>
+                        <h2>Não foram encontrados things</h2>
+                    </div>
+                    @endif 
+                    <div class="row justify-content-center">
+                        <div class="pagination" align="middle"> {{$things->links()}}</div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 </main>
 @endsection
